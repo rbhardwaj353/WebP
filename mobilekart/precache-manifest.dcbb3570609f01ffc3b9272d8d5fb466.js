169ae4342ca7ceb4842a83c0002dd761f20c8a0f
Rohit Bhardwaj <rbhardwaj353@gmail.com>
2020-08-07 00:11:41 +0530

    new files

---
+++ mobilekart/precache-manifest.dcbb3570609f01ffc3b9272d8d5fb466.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "548a259f0db04dfb0f726d157c7b81f9",
+    "url": "/WebP/mobilekart/index.html"
+  },
+  {
+    "revision": "12eb8ba1c4abb0b0eb77",
+    "url": "/WebP/mobilekart/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e8b72365019589cfbf10",
+    "url": "/WebP/mobilekart/static/css/main.50058a87.chunk.css"
+  },
+  {
+    "revision": "12eb8ba1c4abb0b0eb77",
+    "url": "/WebP/mobilekart/static/js/2.3504824a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WebP/mobilekart/static/js/2.3504824a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8b72365019589cfbf10",
+    "url": "/WebP/mobilekart/static/js/main.53a58d4c.chunk.js"
+  },
+  {
+    "revision": "21d96978ef0694ee42bf",
+    "url": "/WebP/mobilekart/static/js/runtime-main.cd87e37a.js"
+  }
+]);
